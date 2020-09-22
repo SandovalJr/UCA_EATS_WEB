462d484a174b216858fa405b6f7e0705920d801f
Eduardo Sandoval <edubizzle7@gmail.com>
2020-09-22 09:39:48 -0500

    archivo de rutas

---
+++ UcaEats_Angular/src/app/routes/routes.ts
@@ -0,0 +1,8 @@
+import { RouterModule, Routes } from "@angular/router";
+import { Component } from "@angular/core";
+
+
+
+const routes: Routes = []
+
+export const APP_ROUTES = RouterModule.forRoot(routes);
