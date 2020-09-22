5dcaf4a08463aadee91dedd384155d5aa568171f
Eduardo Sandoval <edubizzle7@gmail.com>
2020-09-22 09:43:48 -0500

    importo a modulo las rutas

--- UcaEats_Angular/src/app/app.module.ts
+++ UcaEats_Angular/src/app/app.module.ts
@@ -1,30 +1,33 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { FormsModule } from "@angular/forms";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { NavbarComponent } from "./components/inicio/partials/navbar/navbar.component";
+import { FooterComponent } from "./components/inicio/partials/footer/footer.component";
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { NavbarComponent } from './components/inicio/partials/navbar/navbar.component';
-import { FooterComponent } from './components/inicio/partials/footer/footer.component';
+// RUTAS
+import { APP_ROUTES } from "./routes/routes";
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    APP_ROUTES,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
