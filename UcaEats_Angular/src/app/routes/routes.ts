f777be5725c4bab34f786208d574822956b1238b
Eduardo Sandoval <edubizzle7@gmail.com>
2020-09-22 10:17:49 -0500

    se intento agregar el log in pero se ve feo por que no agarra el js

--- UcaEats_Angular/src/app/routes/routes.ts
+++ UcaEats_Angular/src/app/routes/routes.ts
@@ -3,7 +3,15 @@ import { Component } from "@angular/core";
 
 // Inicio
 import { InicioComponent } from "../components/inicio/inicio.component";
+// LogIn
+import { LoginComponent } from "../components/sw/login/login.component";
 
-const routes: Routes = [{ path: "", component: InicioComponent }];
+const routes: Routes = [
+  { path: "", component: InicioComponent },
+  {
+    path: "LogIn_UcaMeals",
+    component: LoginComponent,
+  },
+];
 
 export const APP_ROUTES = RouterModule.forRoot(routes);
