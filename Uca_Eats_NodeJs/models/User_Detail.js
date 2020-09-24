57ea81fe42026d01276b73e734d2aea17876c18b
Eduardo Sandoval <edubizzle7@gmail.com>
2020-09-23 20:41:15 -0500

    creacion de ruta users details

--- Uca_Eats_NodeJs/models/User_Detail.js
+++ Uca_Eats_NodeJs/models/User_Detail.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize"); //para usar el modelo del user
 const db = require("../database/db.js");
 //datos de nuestra BD
 module.exports = db.sequelize.define(
-  "usuario",
+  "user_detail",
   {
     user_id: {
       type: Sequelize.INTEGER,
