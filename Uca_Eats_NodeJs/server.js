7a16a3d997e1128024e79d5b829a843c71bab47d
Eduardo Sandoval <edubizzle7@gmail.com>
2020-09-23 20:27:27 -0500

    se agrego el gitignore por que que flojera descargar todos lo del json jaja

---
+++ Uca_Eats_NodeJs/server.js
@@ -0,0 +1,29 @@
+let express = require("express");
+let cors = require("cors");
+let bodyParser = require("body-parser");
+let app = express();
+const morgan = require("morgan");
+// Settings
+let port = process.env.PORT || 3000; //puerto donde correrá
+
+
+app.use(cors());
+
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: false
+//     })
+// );
+
+// Middlewares
+app.use(morgan("dev"));
+//app.use('/usuarios', Users);
+app.use(express.json());
+
+//ROUTES--------------------------------------------------------------
+
+app.use("/api/user_detail", require("./routes/User_Details"));
+
+app.listen(port, function () {
+  console.log("Server is running on port: " + port);
+});
